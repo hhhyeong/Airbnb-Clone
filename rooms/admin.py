65903a29212303dd9243503761c7abcba45fdf72
hhhyeong <hye_zero.jeong@kt.com>
2020-09-15 19:14:34 +0900

    #Q.1) RoomAdmin에서 queryset 관련 질문 - obj의 정체

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,7 +7,12 @@ from . import models
 # => ItemAdmin에 등록된 모델들은 추상모델을 상속받아서 모두 DB에 테이블로 추가되는 대상이 아닌 애들인가봄!
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "used_by")
+
+    # Item을 사용하고 있는 Room의 개수?
+    def used_by(self, obj):
+        return obj.rooms.count()
+
 
 
 # Room
@@ -48,6 +53,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -68,8 +74,15 @@ class RoomAdmin(admin.ModelAdmin):
 
 
     # admin panel의 목록에 보여지는 컬럼에 연산 함수도 추가 가능.
+    # queryset 사용) Amenity 객체의 개수 반환.
     def count_amenities(self, obj):
         return obj.amenities.count()
+
+    # queryset 사용) Photo 객체의 개수 반환.
+    #               => models.Photo 를 상속한것도 아닌데, Photo모델을 받을수 있나보지???
+    #                  obj가 어떤의미일까. models.py 전부를 말하는걸까????
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 
